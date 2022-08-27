6a1048e14a80b967a5c31317cc17dbfdfa37c387
kardas-bp <kardasbp@gmail.com>
2022-08-27 15:36:36 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import { Text } from './components'
+import Emphasis from './components/Emphasis'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Text as='h1'>Hello CodeSandbox</Text>
+      <Text as='h2' color='violet' style={{ backgroundColor: 'black' }}>
+        Start editing to see some magic happen!
+      </Text>
+      <Text>Start editing to see some magic happen!</Text>
+      <br />
+      <Text as={Emphasis}>This is important, really!</Text>
     </div>
-  );
+  )
 }
-
-export default App;
